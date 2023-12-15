0611b780a57b23808a53d42a3448ff2eb8e1e8d7
bogdanwoj <you@example.combogdanwojcicki31@gmail.com>
2023-12-15 13:54:23 +0200

    added edit and delete for product images and products into admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,9 @@ Route::middleware('auth')->group(function () {
 
         Route::get('/products', [ProductController::class, 'index'])->name('admin.products.index');
         Route::post('/products/store', [ProductController::class, 'store'])->name('admin.products.store');
+        Route::delete('/products/image/{id}', [ProductController::class, 'deleteImage'])->name('admin.products.image.delete');
+        Route::delete('products/destroy/{id}', [ProductController::class, 'destroy'])->name('admin.products.destroy');
+        Route::put('/products/update/{id}', [ProductController::class, 'update'])->name('admin.products.update');
 
 
     });
