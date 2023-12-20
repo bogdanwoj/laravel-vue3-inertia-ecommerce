e8622fb4a432fdeb6889fb9b69b2bb6d1175d99a
bogdanwoj <you@example.combogdanwojcicki31@gmail.com>
2023-12-20 14:10:01 +0200

    added auth user orders view

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -9,9 +9,16 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    function index()
+    public function index()
     {
-        $orders = Order::with('order_items.product.brand', 'order_items.product.category')->get();
-        return Inertia::render('User/Dashboard', ['orders' => $orders]);
+        // Get orders created by the authenticated user
+        $ordersBy = Order::where('created_by', auth()->user()->id)->get();
+
+        // Get all orders with details
+        $orders = Order::with('order_items.product.brand', 'order_items.product.category')
+                       ->where('created_by', auth()->user()->id)
+                       ->get();
+
+        return Inertia::render('User/Dashboard', ['orders' => $orders, 'ordersBy' => $ordersBy]);
     }
 }
--- resources/js/Pages/User/Dashboard.vue
+++ resources/js/Pages/User/Dashboard.vue
@@ -2,14 +2,21 @@
     <UserLayout>
 
 
-        <div class="relative max-w-screen-xl py-20 mt-5 mx-auto overflow-x-auto  ">
-            <table v-show="order.order_items.length > 0" v-for="order in orders" :key="order.id"
+        <div v-for="order in orders" :key="order.id" class="relative max-w-screen-xl p-4 m-5 mx-auto overflow-x-auto border-4 rounded-[2rem]">
+            <div class="px-6 py-3 text-2xl">
+                Order ID #
+                <b>
+                    {{ order.id }}
+                </b>
+                <div>
+                    At: {{ formatDate(order.created_at) }}
+                </div>
+            </div>
+            <table v-show="order.order_items.length > 0"
                    class="w-full text-left text-gray-100 dark:text-gray-400 mb-10 bg-green-200 rounded"  >
                 <thead class="text-xs text-gray-700 uppercase  dark:bg-gray-700 dark:text-gray-400  whitespace-nowrap">
                 <tr class="text-red-600" >
-                    <th scope="col" class="px-6 py-3 text-2xl">
-                        Order ID # <b>{{ order.id }}</b>
-                    </th>
+
 
                 </tr>
                 <tr class="text-red-400" >
@@ -34,7 +41,7 @@
                 <tbody>
                 <tr v-for="(item, index) in order.order_items" :key="item.id"
                     class="bg-green-100 border-b-2 dark:bg-gray-800 dark:border-gray-700">
-                    <td scope="row" class="px-6 py-4 font-sm text-gray-900 dark:text-white">
+                    <td scope="row" class="px-6 py-4 font-sm text-gray-900 dark:text-white" >
                         {{index + 1}}.
                     </td>
                     <td scope="row" class="px-6 py-4 font-large text-gray-900 dark:text-white">
@@ -63,6 +70,13 @@
     import UserLayout from './Layouts/UserLayout.vue';
 
     defineProps({
-        orders: Array
-    })
+        orders: Array,
+        ordersBy: Array
+    });
+
+    const formatDate = (dateString) => {
+        const options = { day: '2-digit', month: '2-digit', year: 'numeric' };
+        const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
+        return formattedDate;
+    };
 </script>
